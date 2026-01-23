2a0a26b65201a3d9c9b3107d5e62da3f4fccaef7
Daniel <daniel@funktional.dev>
2026-01-23 12:06:52 +0000

    feat: add nix package output

--- flake.nix
+++ flake.nix
@@ -12,6 +12,32 @@
         pkgs = import nixpkgs { inherit system; };
       in
       {
+        packages.default = pkgs.buildNpmPackage {
+          pname = "crypto-api-fetcher";
+          version = "0.1.0";
+
+          src = ./.;
+
+          npmDepsHash = "sha256-ytD+xF0gMxs+ST1b+SXih2F2YHXdo/C7kv43GBDVwk0=";
+
+          nativeBuildInputs = [ pkgs.typescript ];
+
+          buildPhase = ''
+            npm run build
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin $out/lib
+            cp -r dist $out/lib/dist
+            cp -r node_modules $out/lib/node_modules
+            
+            # Create a wrapper script
+            echo "#!${pkgs.runtimeShell}" > $out/bin/crypto-api-fetcher
+            echo "${pkgs.nodejs_24}/bin/node $out/lib/dist/index.js" >> $out/bin/crypto-api-fetcher
+            chmod +x $out/bin/crypto-api-fetcher
+          '';
+        };
+
         devShells.default = pkgs.mkShell {
           packages = [ pkgs.nodejs_24 ];
         };
